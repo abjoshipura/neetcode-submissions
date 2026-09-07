916204a906cd071eddca3f2dc15fd2e6c96b4fba
Akshara Joshipura <53368231+abjoshipura@users.noreply.github.com>
2026-09-06 20:16:36 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if not s or not t or len(s) != len(t):
+            return False
+        
+        freq_map = defaultdict(int)
+        for char in s:
+            freq_map[char] += 1
+        
+        for char in t:
+            freq_map[char] -= 1
+        
+        return not any(freq_map.values())
