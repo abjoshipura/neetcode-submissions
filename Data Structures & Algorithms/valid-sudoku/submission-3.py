aa609629552b9c25a35f7b781d4910c171fa1f3f
Akshara Joshipura <53368231+abjoshipura@users.noreply.github.com>
2026-09-15 11:28:02 -0400

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        visitedRows = defaultdict(set)
+        visitedCols = defaultdict(set)
+        visitedBoxes = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                value = board[i][j]
+                if value == '.':
+                    continue
+                
+                if value in visitedRows[i] or value in visitedCols[j] or value in visitedBoxes[(i // 3, j // 3)]:
+                    return False
+
+                visitedRows[i].add(board[i][j])
+                visitedCols[j].add(board[i][j])
+                visitedBoxes[(i // 3, j // 3)].add(board[i][j])
+
+        return True
